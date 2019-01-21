52569621f78af231f580694ed71b310c3f487e9f
hcstephencheung <hc.stephen.cheung@gmail.com>
2019-01-20 17:55:46 -0800

    Create env variables

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 require('dotenv').load();
 
-var client_id = ''; // Your client id
-var client_secret = ''; // Your secret
+var client_id = '' || process.env.CLIENT_ID; // Your client id
+var client_secret = '' || process.env.CLIENT_SECRET; // Your secret
 var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
 
 /**
